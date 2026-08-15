c64eb890b037bf8d21b4482e9963d95a41da445d
Ashen Ranasinghe <asshenk720@gmail.com>
2026-08-15 11:03:34 +0530

    Refactor Playwright configuration and add VSCode settings and tasks
    - Simplified Playwright reporter configuration by removing 'open' option from HTML reporter.
    - Added VSCode settings to disable automatic npm detection.
    - Created a tasks.json file for managing common npm commands related to testing and reporting.

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,8 +16,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
   reporter: [
-    ['html', { open: 'never' }],
-    ['list'],
+    ['html'],
     ['allure-playwright'],
   ],
   use: {
